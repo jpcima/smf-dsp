2b71671b7d3c478d8d473f0e96de027d6f0501dc
Jean Pierre Cimalando <jp-dev@inbox.ru>
2020-06-27 18:37:02 +0200

    Have the midi sequences separate

--- sources/player/instrument.cc
+++ sources/player/instrument.cc
@@ -4,6 +4,7 @@
 //          http://www.boost.org/LICENSE_1_0.txt)
 
 #include "instrument.h"
+#include "sequences.h"
 #include "utility/logs.h"
 
 Midi_Instrument::Midi_Instrument()
@@ -19,35 +20,14 @@ void Midi_Instrument::send_message(const uint8_t *data, unsigned len, double ts,
 
 void Midi_Instrument::initialize()
 {
-    for (unsigned c = 0; c < 16; ++c) {
-        // all sound off
-        { uint8_t msg[] { (uint8_t)((0b1011 << 4) | c), 120, 0 };
-            send_message(msg, sizeof(msg), 0, 0); }
-        // reset all controllers
-        { uint8_t msg[] { (uint8_t)((0b1011 << 4) | c), 121, 0 };
-            send_message(msg, sizeof(msg), 0, 0); }
-        // volume
-        { uint8_t msg[] { (uint8_t)((0b1011 << 4) | c), 7, 100 };
-            send_message(msg, sizeof(msg), 0, 0); }
-        // pan
-        { uint8_t msg[] { (uint8_t)((0b1011 << 4) | c), 10, 64 };
-            send_message(msg, sizeof(msg), 0, 0); }
-        // bank select
-        { uint8_t msg[] { (uint8_t)((0b1011 << 4) | c), 0, 0 };
-            send_message(msg, sizeof(msg), 0, 0); }
-        { uint8_t msg[] { (uint8_t)((0b1011 << 4) | c), 32, 0 };
-            send_message(msg, sizeof(msg), 0, 0); }
-        // program change
-        { uint8_t msg[] { (uint8_t)((0b1100 << 4) | c), 0 };
-            send_message(msg, sizeof(msg), 0, 0); }
-    }
+    play_initialization_sequence([this](const uint8_t *msg, unsigned len) {
+        send_message(msg, len, 0, 0);
+    });
 }
 
 void Midi_Instrument::all_sound_off()
 {
-    for (unsigned c = 0; c < 16; ++c) {
-        // all sound off
-        { uint8_t msg[] { (uint8_t)((0b1011 << 4) | c), 120, 0 };
-            send_message(msg, sizeof(msg), 0, 0); }
-    }
+    play_all_sound_off([this](const uint8_t *msg, unsigned len) {
+        send_message(msg, len, 0, 0);
+    });
 }
---
+++ sources/player/sequences.h
@@ -0,0 +1,44 @@
+//          Copyright Jean Pierre Cimalando 2020.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE.md or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+#include <cstdint>
+
+template <class F>
+void play_initialization_sequence(F &&play)
+{
+    for (unsigned c = 0; c < 16; ++c) {
+        // all sound off
+        { uint8_t msg[] { (uint8_t)((0b1011 << 4) | c), 120, 0 };
+            play(msg, sizeof(msg)); }
+        // reset all controllers
+        { uint8_t msg[] { (uint8_t)((0b1011 << 4) | c), 121, 0 };
+            play(msg, sizeof(msg)); }
+        // volume
+        { uint8_t msg[] { (uint8_t)((0b1011 << 4) | c), 7, 100 };
+            play(msg, sizeof(msg)); }
+        // pan
+        { uint8_t msg[] { (uint8_t)((0b1011 << 4) | c), 10, 64 };
+            play(msg, sizeof(msg)); }
+        // bank select
+        { uint8_t msg[] { (uint8_t)((0b1011 << 4) | c), 0, 0 };
+            play(msg, sizeof(msg)); }
+        { uint8_t msg[] { (uint8_t)((0b1011 << 4) | c), 32, 0 };
+            play(msg, sizeof(msg)); }
+        // program change
+        { uint8_t msg[] { (uint8_t)((0b1100 << 4) | c), 0 };
+            play(msg, sizeof(msg)); }
+    }
+}
+
+template <class F>
+void play_all_sound_off(F &&play)
+{
+    for (unsigned c = 0; c < 16; ++c) {
+        // all sound off
+        { uint8_t msg[] { (uint8_t)((0b1011 << 4) | c), 120, 0 };
+            play(msg, sizeof(msg)); }
+    }
+}
