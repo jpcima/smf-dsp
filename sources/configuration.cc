8cf2ab5b9b56a831e873a19db432b18577a9b92f
Jean Pierre Cimalando <jp-dev@inbox.ru>
2020-07-08 16:24:32 +0200

    Keep the configuration in AppData

--- sources/configuration.cc
+++ sources/configuration.cc
@@ -5,11 +5,45 @@
 
 #include "configuration.h"
 #include "utility/paths.h"
+#include "utility/charset.h"
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <system_error>
 #if defined(_WIN32)
+#include <windows.h>
+#include <shlobj.h>
 #include <direct.h>
 #define mkdir(path, mode) _mkdir(path)
+#endif
+
+#if defined(_WIN32)
+static std::string known_folder_path(int csidl, std::error_code &ec)
+{
+    ec.clear();
+
+    WCHAR wpath[PATH_MAX + 1];
+    HRESULT result = SHGetFolderPathW(nullptr, csidl, nullptr, SHGFP_TYPE_CURRENT, wpath);
+
+    if (result != S_OK) {
+        ec = std::error_code(result, std::system_category());
+        return std::string();
+    }
+
+    wpath[PATH_MAX] = L'\0';
+
+    std::string path;
+    convert_utf<WCHAR, char>(wpath, path, true);
+    return normalize_path_separators(path + '/');
+}
+
+static std::string known_folder_path(int csidl)
+{
+    std::error_code ec;
+    std::string path = known_folder_path(csidl, ec);
+    if (ec)
+        throw std::system_error(ec);
+    return path;
+}
 #endif
 
 const std::string &get_configuration_dir()
@@ -17,9 +51,8 @@ const std::string &get_configuration_dir()
     static const std::string path = []() -> std::string {
         std::string path;
 #if defined(_WIN32)
-        path = get_executable_path();
-        while (!path.empty() && path.back() != '/') path.pop_back();
-        path.append("config/");
+        path = known_folder_path(CSIDL_LOCAL_APPDATA|CSIDL_FLAG_CREATE);
+        path.append(PROGRAM_DISPLAY_NAME "/");
         mkdir(path.c_str(), 0755);
 #elif defined(__HAIKU__)
         path = get_home_directory();
