a0c9a5a07e8406954212648d4a165c15c5d4f0eb
Jean Pierre Cimalando <jp-dev@inbox.ru>
2021-12-31 22:34:30 +0100

    Update all copyright years [ci skip]

--- sources/player/playlist.cc
+++ sources/player/playlist.cc
@@ -1,9 +1,4 @@
-//          Copyright Jean Pierre Cimalando 2019.
-// Distributed under the Boost Software License, Version 1.0.
-//    (See accompanying file LICENSE.md or copy at
-//          http://www.boost.org/LICENSE_1_0.txt)
-
-//          Copyright Jean Pierre Cimalando 2018.
+//          Copyright Jean Pierre Cimalando 2018-2022.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE.md or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
--- sources/utility/SDL_stb_image.c
+++ sources/utility/SDL_stb_image.c
@@ -1,4 +1,4 @@
-//          Copyright Jean Pierre Cimalando 2020.
+//          Copyright Jean Pierre Cimalando 2020-2022.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE.md or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
